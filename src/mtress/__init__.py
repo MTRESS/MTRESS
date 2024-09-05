fa4793117a15db2846f20cecc94a275c525655b4
Ellermann <julius.ellermann@dlr.de>
2024-09-05 08:05:02 +0200

    rearrangement of site structure

--- src/mtress/__init__.py
+++ src/mtress/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 """
+.. include:: ../../README.md
+"""
+
+"""
 SPDX-FileCopyrightText: Deutsches Zentrum für Luft und Raumfahrt
 SPDX-FileCopyrightText: Patrik Schönfeldt
 
@@ -10,10 +14,6 @@ from ._abstract_component import SolphLabel
 from ._location import Location
 from ._meta_model import Connection, MetaModel
 from ._solph_model import SolphModel
-
-"""
-.. include:: ../../README.md
-"""
 
 __version__ = "3.0.0a2"
 
