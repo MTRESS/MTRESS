4483400a7b6469e832e416c83983b4714ed2378d
Ellermann <julius.ellermann@dlr.de>
2024-09-05 08:18:23 +0200

    rearrange __init__

--- src/mtress/__init__.py
+++ src/mtress/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
+"""
+.. include:: ../../README.md
+"""
+
 from ._abstract_component import SolphLabel
 from ._location import Location
 from ._meta_model import Connection, MetaModel
 from ._solph_model import SolphModel
-
-"""
-.. include:: ../../README.md
-"""
 
 """
 SPDX-FileCopyrightText: Deutsches Zentrum für Luft und Raumfahrt
