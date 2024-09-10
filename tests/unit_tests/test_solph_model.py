11aba16517d6026bcd936aa7065170db57a4b201
Ellermann <julius.ellermann@dlr.de>
2024-09-10 10:48:59 +0200

    test for solph_model graph function: check for nodes

--- tests/unit_tests/test_solph_model.py
+++ tests/unit_tests/test_solph_model.py
@@ -4,11 +4,19 @@ Tests for the MTRESS solph model.
 """
 
 import datetime
+import json
 
 import pandas as pd
 import pytest
 
-from mtress import Connection, Location, MetaModel, SolphModel, carriers
+from mtress import (
+    Connection,
+    Location,
+    MetaModel,
+    SolphModel,
+    carriers,
+    demands,
+)
 from mtress.technologies.grid_connection import ElectricityGridConnection
 
 
@@ -97,4 +105,71 @@ def test_build_model_with_connected_electricity_missing_connection():
                 "end": "2021-07-10 15:15:00",
                 "freq": "15T",
             },
+        )
+
+
+def test_graph():
+    nodes = []
+    meta_model = MetaModel()
+
+    name = "house_1"
+    house_1 = Location(name=name)
+    meta_model.add_location(house_1)
+
+    carrier0 = carriers.ElectricityCarrier()
+    nodes.append(("house_1", "ElectricityCarrier", "distribution"))
+    nodes.append(("house_1", "ElectricityCarrier", "feed_in"))
+
+    carrier1 = carriers.HeatCarrier(
+        temperature_levels=[10, 20, 30],
+        reference_temperature=0,
+    )
+    nodes.append(("house_1", "HeatCarrier", "missing_heat"))
+    nodes.append(("house_1", "HeatCarrier", "excess_heat"))
+    nodes.append(("house_1", "HeatCarrier", "T_10"))
+    nodes.append(("house_1", "HeatCarrier", "T_20"))
+    nodes.append(("house_1", "HeatCarrier", "T_30"))
+
+    demand1 = demands.Electricity(name="demand1", time_series=[0, 1, 2])
+    nodes.append(("house_1", "demand1", "input"))
+    nodes.append(("house_1", "demand1", "sink"))
+
+    demand2 = demands.FixedTemperatureHeating(
+        name="demand2",
+        min_flow_temperature=20,
+        return_temperature=10,
+        time_series=[1, 2, 3],
+    )
+    nodes.append(("house_1", "demand2", "output"))
+    nodes.append(("house_1", "demand2", "sink"))
+    nodes.append(("house_1", "demand2", "heat_exchanger"))
+
+    house_1.add(carrier0)
+    house_1.add(carrier1)
+    house_1.add(demand1)
+    house_1.add(demand2)
+
+    solph_representation = SolphModel(
+        meta_model,
+        timeindex={
+            "start": "2021-07-10 00:00:00",
+            "end": "2021-07-10 03:00:00",
+            "freq": "60T",
+        },
+    )
+
+    solph_representation.build_solph_model()
+
+    plot = solph_representation.graph(detail=True)
+
+    plot_json_string = plot.pipe("json").decode()
+    plot_json_dict = json.loads(plot_json_string)
+
+    assert plot_json_dict["name"] == "MTRESS model"
+
+    obj_names = [obj["name"] for obj in plot_json_dict["objects"]]
+    for n in nodes:
+        assert (
+            f"SolphLabel(location='{n[0]}', mtress_component='{n[1]}', solph_node='{n[2]}')"
+            in obj_names
         )
