5e482dea2ba64479371a13f5f7a098e8383294a7
Pedro Lacerda de Magalhães <pedro.magalhaes@dlr.de>
2024-09-16 10:13:02 +0200

    Added test for the HeatPump class using custom design conditions.

--- tests/unit_tests/test_technologies/test_heat_pump.py
+++ tests/unit_tests/test_technologies/test_heat_pump.py
@@ -14,3 +14,23 @@ def test_heat_pump_initialisation():
     assert hp.name == hp_name
     assert hp.thermal_power_limit == hp_thermal_power_limit
     assert hp.ref_cop == hp_ref_cop
+
+def test_heat_pump_init_custom_design_conditions():
+    hp_name = "test_hp"
+    hp_thermal_power_limit = 4e3  # W
+    hp_ref_cop = 3.6
+
+    hp = HeatPump(
+        name=hp_name,
+        thermal_power_limit=hp_thermal_power_limit,
+        ref_cop=hp_ref_cop,
+        ref_temp_secondary_out=40,
+        ref_temp_secondary_in=25,
+        ref_temp_primary_in=5,
+        ref_temp_primary_out=-10
+
+    )
+
+    assert hp.name == hp_name
+    assert hp.thermal_power_limit == hp_thermal_power_limit
+    assert hp.ref_cop == hp_ref_cop
